0fa1785c720eda7f04e270d4f71dbe171234c4e1
lihongm2@UTORONTO.CA <lihongm2@UTORONTO.CA@a184c211-6917-47c9-80ea-094f3466f46b>
2018-10-27 20:06:07 +0000

    Class File is added in

---
+++ Assignment2/src/a2/File.java
@@ -0,0 +1,23 @@
+package a2;
+
+public class File {
+	
+	private String name;
+	private String content;
+	
+	public File(String name) {
+		this.name = name;
+		this.content = "";
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+	
+}
