b51e03851c8085acb2b838e43b7a7c20c508dffe
mihao <mihao@UTORONTO.CA>
2018-10-28 02:13:37 +0000

    The super class of File and Directory

---
+++ src/a2/FileDirectory.java
@@ -0,0 +1,34 @@
+package a2;
+
+public class FileDirectory {
+	private String name;
+	private FileDirectory parent;
+
+	public FileDirectory(String name, FileDirectory parent){
+		this.name = name;
+		this.parent = parent;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public void setParent(Directory parent){
+		this.parent = parent;
+	}
+	
+	public FileDirectory getParent(){
+		return this.parent;
+	}
+	
+	public String toString(){
+		String output = this.name;
+		FileDirectory currParent = this.parent;
+		
+		while (currParent != null){
+			output = currParent.getName()+"\\"+output;
+			currParent = currParent.getParent();
+		}
+		return output;
+	}
+}
