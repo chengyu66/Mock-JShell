7239917d88656ea7696baf134c62b227bf6b7ed2
wangy809@UTORONTO.CA <wangy809@UTORONTO.CA@a184c211-6917-47c9-80ea-094f3466f46b>
2018-10-28 22:01:41 +0000

    write the code of command class

--- Assignment2/src/a2/Command.java
+++ Assignment2/src/a2/Command.java
@@ -1,13 +1,19 @@
 package a2;
 
 public class Command {
+  public String fpart;
+  public String spart;
   
   private static String[] validCommands =
     {"exit", "mkdir", "cd", "is", "pwd", "pushd", "popd", "history", "cat",
         "echo", "man"};
   
-  public Command() {
-    
+  public Command(String input) {
+    String[] my_list = input.split(" ");
+    fpart = my_list[0];
+    if(this.isValid(input)){
+      this.run(fpart);
+    }
   }
   
   public String[] getValidCommands() {
@@ -15,9 +21,25 @@ public class Command {
   }
   
   // every subclass of Command should override this method
-  public void run() {}
+  public void run(String cmd) {
+    if(cmd == "cd") {
+      a2.ChangeDirectory c = new ChangeDirectory(cmd);
+    }
+    // other cases writing here
+  }
   
-  public boolean isValid() {
-    return true;
+  public boolean isValid(String input) {
+    boolean result = false;
+    if(fpart.isEmpty()) {
+      result = false;
+    } else {
+      for(String str:validCommands) {
+        if(str == fpart) {
+          result = true;
+        }
+      }
+      result = true;
+    }
+    return result;
   }
 }
