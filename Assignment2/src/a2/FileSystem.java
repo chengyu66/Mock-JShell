e2e6a3308ec180ccf9be8d0d01d576a65c8a2848
mihao@UTORONTO.CA <mihao@UTORONTO.CA@a184c211-6917-47c9-80ea-094f3466f46b>
2018-10-29 18:02:51 +0000

    Fix the error

--- Assignment2/src/a2/FileSystem.java
+++ Assignment2/src/a2/FileSystem.java
@@ -1,12 +1,14 @@
 package a2;
 
+import java.util.ArrayList;
+
 public class FileSystem {
 	
 	private Directory root;
 	private Directory currentDirectory;
 	
 	public FileSystem(String nameOfRoot) {
-		this.root = Directory(nameOfRoot, null);
+		this.root = new Directory(nameOfRoot, null);
 		this.currentDirectory = root;
 		
 	}
@@ -20,23 +22,26 @@ public class FileSystem {
 		return this.currentDirectory;
 	}
 	
+	public Directory getRoot(){
+		return this.root;
+	}
+	
 	// this method is additional.
 	// you pass in a path, FileSystem get to that directory
-	public void Trace(String path) {
+	// need to consider the case of File.
+	public Directory Trace(String path) {
 		// Decompose the string <path> into the name of each folder
 		String[] folders = path.split("/");
-		
+
 		// The directory currently getting examined
-		Directory curr = FileSystem.getRoot();
+		Directory curr = this.getRoot();
 				
 		// loop through the folders
-		For (String folderName: folders);{
+		for (String folderName: folders){
 			int index = curr.findSub(folderName);
 			// Directory doesn't exist
 			if (index != -1) {
-				ArrayList<FileDirectory> currList = curr.findSub(folderName);
-				
-				curr = currList.getIndex(index);
+				curr = (Directory) curr.getSub().get(index);
 			}
 		}
 		return curr;
