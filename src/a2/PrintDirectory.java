63ea7fb3c61a210943ee09f02286beeff1672642
wangy809 <wangy809@UTORONTO.CA>
2018-10-29 22:53:42 +0000

    fix PrintDirectory class

--- src/a2/PrintDirectory.java
+++ src/a2/PrintDirectory.java
@@ -1,22 +1,17 @@
 package a2;
-import a2.FileDirectory;
 import a2.FileSystem;
 
 public class PrintDirectory extends Command{
-  public FileSystem all;
-  
-  
-  public PrintDirectory(String input) {
-    super(input);
-    FileDirectory dir = all.getCurrentDirectory();
-    String str = null;
-    while (dir!=null) {
-      str += dir.getName();
-      dir = dir.getParent();
-      if (dir!=null) {
-        str += "/";
-      }
+
+  public PrintDirectory(FileSystem fs) {
+    super(fs);
+  }
+
+  public void run(String[] input) {
+    if (fs!= null) {
+      System.out.println(fs.getCurrentDirectory());
+    }else {
+      System.out.println("FileSystem not created");
     }
-    System.out.println(str);
   }
 }
