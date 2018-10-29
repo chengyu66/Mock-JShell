2de70603e31677bfc88246b25edbe2b4e69e5524
mihao <mihao@UTORONTO.CA>
2018-10-29 20:12:44 +0000

    Add some code in class

--- src/a2/History.java
+++ src/a2/History.java
@@ -2,22 +2,23 @@ package a2;
 
 import java.util.ArrayList;
 
-public class History {
+public class History extends Command{
 	private ArrayList<String> historyList;
 
-	public History()
+	public History(FileSystem fs)
 	{
+		super(fs);
 		historyList = new ArrayList<String>();
 	}
 	
-	public void addHistory(String newHistory)
+	public void addHistory(String [] newHistory)
 	{
-		historyList.add((historyList.size()+1)+ ". " + newHistory);
+		historyList.add((historyList.size()+1)+ ". " + String.join(" ", newHistory));
 	}
 	
-	public void printHistory(int numbLine)
+	public void run(String [] input)
 	{
-		for (int i = numbLine;i<historyList.size(); i++)
+		for (int i = Integer.parseInt(input[1]);i<historyList.size(); i++)
 		{
 			System.out.println(historyList.get(i));
 		}
