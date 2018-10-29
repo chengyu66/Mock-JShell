a435ee3c12159ad396d4d30faae5269f07083a2c
wangy809@UTORONTO.CA <wangy809@UTORONTO.CA@a184c211-6917-47c9-80ea-094f3466f46b>
2018-10-29 22:19:51 +0000

    fix ChangeDirectory(cd) class

--- Assignment2/src/a2/ChangeDirectory.java
+++ Assignment2/src/a2/ChangeDirectory.java
@@ -1,45 +1,16 @@
 package a2;
-import a2.FileDirectory;
-import a2.Directory;
+
 import a2.FileSystem;
-import java.util.ArrayList;
 
 public class ChangeDirectory extends Command{
-  public Directory dir;
-  public FileSystem all;
-  
-  
-  public ChangeDirectory(String input) {
-    super(input);
-    String[] folder = spart.split("/");
-    dir = this.findDir(folder[-1]);
-    all.setCurrentDirectory(dir);
+
+  public ChangeDirectory(FileSystem fs) {
+    super(fs);
     
   }
-  public Directory findDir(String name) {
-    Directory result = null;
-    Directory cur = all.getRoot();
-    ArrayList<Directory> sublist = null;
-    if (cur!= null) {
-      if (cur.getName()==name) {
-        result = cur;
-      }else {
-        sublist = cur.getSub();
-        while (sublist!=null) {
-          for(Directory sub:cur.getSub()) {
-            if (sub.getName()==name) {
-              result = sub;
-            }else {}
-          }
-          cur = sublist.remove(0);
-          for (Directory sub:cur.getSub()) {
-            if (sub != null) {
-              sublist.add(0, sub);
-            }
-          }
-        } 
-      }
-    }
-    return result;
+  public void run(String[] input) {
+    String path = input[1]; 
+    Directory dir = fs.trace(path);
+    fs.setCurrentDirectory(dir);
   }
 }
