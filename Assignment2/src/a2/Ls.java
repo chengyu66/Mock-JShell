9199eb49b48c27961baa8cf065d870e3db055603
xincheng@UTORONTO.CA <xincheng@UTORONTO.CA@a184c211-6917-47c9-80ea-094f3466f46b>
2018-10-30 16:05:59 +0000

    update Ls Class

--- Assignment2/src/a2/Ls.java
+++ Assignment2/src/a2/Ls.java
@@ -2,25 +2,7 @@ package a2;
 
 
 public class Ls extends Command{
-	
-  /*
-	public Ls() {
-		
-		// get the current directory
-		Directory currentPath = FileSystem.getCurrentDirectory();
-		
-		// print all the contents(file and directory) in current directory
-		for (FileDirectory contents: currentPath.subItem) {
-			System.out.println(contents.getName());
-		}
-	
-	}
-	public Ls(String path) {
-		
-		// I will do this later, get Trace done first
-		Directory targetPath = FileSystem.trace(path);
-		
-	}*/
+  
   public Ls(FileSystem fs) {
     super(fs);
   }
