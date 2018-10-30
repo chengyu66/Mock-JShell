e93049ede9bcfd7b2342d33285a256ce84a4131e
wangy809 <wangy809@UTORONTO.CA>
2018-10-30 04:20:31 +0000

    update ChangeDirectory class with error message

--- src/a2/ChangeDirectory.java
+++ src/a2/ChangeDirectory.java
@@ -9,8 +9,26 @@ public class ChangeDirectory extends Command{
     
   }
   public void run(String[] input) {
-    String path = input[1]; 
-    Directory dir = fs.trace(path);
-    fs.setCurrentDirectory(dir);
+    if (this.isCorrect(input)) {
+      String path = input[1]; 
+      Directory dir = fs.trace(path);
+      if (dir != null){
+      fs.setCurrentDirectory(dir);
+      }
+    }else {
+      System.out.println(errorMessage());
+    }
+  }
+  public String errorMessage() {
+    return "No such file or directory";
+  }
+  public boolean isCorrect(String[] input) {
+    boolean result = false;
+    if (input.length>=2) {
+      result = true;
+    }else {
+      result = false;
+    }
+    return result;
   }
 }
