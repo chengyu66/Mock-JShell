62287a08ee73ffafd4a741c57b3cb7a2e9e0fac2
xincheng@UTORONTO.CA <xincheng@UTORONTO.CA@a184c211-6917-47c9-80ea-094f3466f46b>
2018-10-31 03:44:44 +0000

    Update ChangeDirectory to work with cd .. and cd . Command

--- Assignment2/src/a2/ChangeDirectory.java
+++ Assignment2/src/a2/ChangeDirectory.java
@@ -10,14 +10,22 @@ public class ChangeDirectory extends Command{
   }
   public void run(String[] input) {
     if (this.isValid(input)) {
-      String path = input[1]; 
-      FileDirectory fd = fs.trace(path);
-      if (fd instanceof File) {
-        System.out.println("Not a directory");
-      } else if (fd instanceof Directory) {
-        fs.setCurrentDirectory((Directory)fd);
+      String path = input[1];
+      if (path.equals(".")) {// do nothing
+      } else if (path.equals("..")) {
+        Directory currentDirectory = fs.getCurrentDirectory();
+        if (currentDirectory != fs.getRoot()) {
+          fs.setCurrentDirectory((Directory) currentDirectory.getParent());
+        }
       } else {
-        System.out.println(errorMessage());
+        FileDirectory fd = fs.trace(path);
+        if (fd instanceof File) {
+          System.out.println("Not a directory");
+        } else if (fd instanceof Directory) {
+          fs.setCurrentDirectory((Directory)fd);
+        } else {
+          System.out.println(errorMessage());
+        }
       }
     }else {
       System.out.println(super.errorMessage());
