49ab8d1a8b3e26799e20a74629c17e9974f46fea
xincheng <45149881+chengyu66@users.noreply.github.com>
2018-10-31 03:01:03 +0000

    update FileSystem's trace method to work with File

--- src/a2/FileSystem.java
+++ src/a2/FileSystem.java
@@ -36,14 +36,14 @@ public class FileSystem {
 	// this method is additional.
 	// you pass in a path, FileSystem get to that directory
 	// need to consider the case of File.
-	public Directory trace(String path) {
+	public FileDirectory trace(String path) {
 		// Decompose the string <path> into the name of each folder
 	    if (path.startsWith("/")) {
 	      path = path.substring(1);
 	    }
 		String[] folders = path.split("/");
 		// The directory currently getting examined
-		Directory curr;
+		FileDirectory curr;
 		if (folders[0].equals(root.getName())) {
 		  curr=root;
 		}else if (currentDirectory.findSub(folders[0])!=-1) {
@@ -53,16 +53,21 @@ public class FileSystem {
 	    }
     // loop through the folders
 		for (String folderName: folders){
-			int index = curr.findSub(folderName);
-			// Directory found
-			if (index != -1) {
-				curr = (Directory) curr.getSub().get(index);
-			}
-			else if (folderName.equals(root.getName())){
-			    curr = root;
-			}else {
-				return null;
-			}
+		  if (curr instanceof Directory) {
+		    int index = ((Directory) curr).findSub(folderName);
+            // Directory found
+            if (index != -1) {
+                curr = ((Directory) curr).getSub().get(index);
+            }
+            else if (folderName.equals(root.getName())){
+                curr = root;
+            }else {
+                return null;
+            }
+		  } else {
+		    System.out.println("File is not a directory");
+		    return null;
+		  }
 		}
 		return curr;
 	}
