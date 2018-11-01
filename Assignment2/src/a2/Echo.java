566a2f6993b37df0260d0ed1a40b146cc9c708f9
mihao@UTORONTO.CA <mihao@UTORONTO.CA@a184c211-6917-47c9-80ea-094f3466f46b>
2018-11-01 02:23:57 +0000

    The echo class which is the subclass of command

---
+++ Assignment2/src/a2/Echo.java
@@ -0,0 +1,46 @@
+package a2;
+
+import java.util.ArrayList;
+
+public class Echo extends Command{
+	public Echo(FileSystem fs)
+	{
+		super(fs);
+	}
+	
+	public void run(String [] input){
+		File currFile = null;
+		Directory currDirect = null;
+		String fileName = input[3].split("/")[input[3].split("/").length-1];
+
+		//Case 1: only STRING in input
+		if (input.length == 2)
+			System.out.println(input[1]);
+		//Case 2: String and OutFile in input
+		else if (input.length == 4){
+			//Case 2.1£º OutFile is a path
+			if (input[3].contains("/")){
+				currFile = (File) fs.trace(input[3]);
+				//Case 2.1.1: file does not exit in OutFile
+				if (currFile == null){
+					currDirect = (Directory)fs.trace(input[3].replace("/"+fileName,""));
+					//Case 2.1.1.1: the directory holds the file does not exist in OutFile
+					if (currDirect == null){
+						//Raise Error
+					}
+					//Case 2.1.1.2: the directory holds the file exists in OutFile
+					else{
+						currFile = new File(fileName, currDirect);
+						currFile.setContent(input[0]);
+						currDirect.setSub(currFile);
+					}
+				}
+				//Case 2.1.2: file exit in OutFile
+				else{
+					currFile.setContent(input[0]);
+				}
+			}
+			//Case 2.2: OutFile is a name.
+		}
+	}
+}
