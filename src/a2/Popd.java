861007d26d199b6bbc0ff29e5e9a7018f77756e1
lihongm2@UTORONTO.CA <lihongm2@UTORONTO.CA>
2018-11-07 22:19:50 +0000

    Popd updated

---
+++ src/a2/Popd.java
@@ -0,0 +1,32 @@
+package a2;
+
+
+public class Popd extends Command{
+	
+	public Popd(FileSystem fs) {
+		super(fs);
+	}
+	
+	public void run(String[] input) {
+		
+		// There is at least one Directory in the DirectoryStack
+		if (!(fs.getDirectoryStack().isEmpty())) {
+			// The directory on the top of the entry
+			Directory topDirectory = fs.getDirectoryStack().pop();
+			// change the directory to the top entry of DirectoryStack
+			fs.setCurrentDirectory(topDirectory);
+			
+		}
+		// No Directory in the stack
+		else {
+			// send a message to the user
+			System.out.println(EmptyDirectoryStackError());
+			
+		}
+	}
+	public String EmptyDirectoryStackError() {
+	    return "DirecotryStack is empty";
+	  }
+
+}
+
