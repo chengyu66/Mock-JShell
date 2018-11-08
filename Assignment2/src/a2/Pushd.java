1056e89baf6531013df585b414260818481a2d91
lihongm2@UTORONTO.CA <lihongm2@UTORONTO.CA@a184c211-6917-47c9-80ea-094f3466f46b>
2018-11-08 02:28:32 +0000

    javadoc

--- Assignment2/src/a2/Pushd.java
+++ Assignment2/src/a2/Pushd.java
@@ -1,19 +1,17 @@
 package a2;
-/**
- * Determines how similar each pair of users is based on their ratings. This
- * similarity value is represented with with a float value between 0 and 1,
- * where 1 is perfect/identical similarity. Stores these values in the
- * userUserMatrix.
- * 
- * @param items1 is the column of the first user.
- * @param items2 is the column of the second user.
- * @return return a double represents the distance between users.
- */
+
 public class Pushd extends Command{
 	
 	public Pushd(FileSystem fs) {
 		super(fs);
 	}
+	/**
+
+	 * Save the current directory and go to the next directory.
+	 * 
+	 * @param array of [Command, String] 
+	 * @return null
+	 */
 	
 	public void run(String[] input) {
 		
