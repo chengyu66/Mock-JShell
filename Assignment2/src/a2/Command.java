d60ae7212f8354ac581dee080fce17e51b56d847
wangy809@UTORONTO.CA <wangy809@UTORONTO.CA@a184c211-6917-47c9-80ea-094f3466f46b>
2018-11-08 02:40:54 +0000

    update command documentation

--- Assignment2/src/a2/Command.java
+++ Assignment2/src/a2/Command.java
@@ -2,27 +2,28 @@ package a2;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-/**
- * Determines how similar each pair of users is based on their ratings. This
- * similarity value is represented with with a float value between 0 and 1,
- * where 1 is perfect/identical similarity. Stores these values in the
- * userUserMatrix.
- * 
- * @param items1 is the column of the first user.
- * @param items2 is the column of the second user.
- * @return return a double represents the distance between users.
- */
+
 public abstract class Command {
   
   public static FileSystem fs;
-
+  /**
+   * Construct a command class with filesystem
+   * 
+   * @param fs is a filesystem already build
+   * @return return None
+   */
   public Command(FileSystem fs) {
     Command.fs = fs;
   }
 
   // Every subClass should override this method
   public void run(String[] input) {}
-
+  /**
+   * tell if the command name has symbol that non-valid
+   * 
+   * @param name is the String that contain Commmand names
+   * @return return true/false
+   */
   public boolean validName(String name) {
     Pattern p = Pattern.compile("[/.!@#$%^&*(){}~|<>?]");
     Matcher m = p.matcher(name);
