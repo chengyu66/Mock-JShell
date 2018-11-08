bbb6ef4ae431f12388538aa838c7073d3f3af8a5
mihao <mihao@UTORONTO.CA>
2018-11-08 03:44:51 +0000

    add some method

--- src/test/Test.java
+++ src/test/Test.java
@@ -8,7 +8,31 @@ public class Test {
 		fs = FileSystem.createInstanceOfFileSystem();
 	}
 	
-	public void testFile(){
+	public static FileDirectory getItem(int number){
+		Directory item1 = new Directory("item1", null);
+		Directory item2 = new Directory("item2", item1);
+		Directory item3 = new Directory("item3", item2);
+		Directory item4 = new Directory("item4", item3);
+		Directory item5 = new Directory("item5", item4);
+		File file1 = new File("file1",item5);
+		
+		if (number == 1)
+			return item1;
+		if (number == 2)
+			return item2;
+		if (number == 3)
+			return item3;
+		if (number == 4)
+			return item4;
+		if (number == 5)
+			return item5;
+		if (number == 6)
+			return file1;
+		else
+			return null;
+			
+	}
+	public static void testFile(){
 		String actual, expect;
 		File file = new File("hello", null);
 		file.setContent("beg some mark QAQ");
@@ -21,8 +45,8 @@ public class Test {
 		expect = "beg some mark QAQ";
 		System.out.println("Check the content in the file: "+ actual.equals(expect));
 	}
-
+	
 	public static void main(String [] args){
-		
+		testFile();
 	}
 }
