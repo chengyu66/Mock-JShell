8b16672269544a4bee36da7b94b61aaa5ffe1fd5
xincheng <45149881+chengyu66@users.noreply.github.com>
2018-11-08 00:17:31 +0000

    Update Command Class

--- src/a2/Command.java
+++ src/a2/Command.java
@@ -1,5 +1,7 @@
 package a2;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public abstract class Command {
   
@@ -12,6 +14,13 @@ public abstract class Command {
   // Every subClass should override this method
   public void run(String[] input) {}
 
+  public boolean validName(String name) {
+    Pattern p = Pattern.compile("[/.!@#$%^&*(){}~|<>?]");
+    Matcher m = p.matcher(name);
+    // boolean b = m.matches();
+    boolean result = m.find();
+    return !result;
+  }
   
   public String errorMessage() {
     return "Wrong Input Format";
