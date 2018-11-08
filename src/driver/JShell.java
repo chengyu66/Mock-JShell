d56901b7be5731d42b6af25839c5c121c63df9c4
wangy809 <wangy809@UTORONTO.CA>
2018-11-08 01:55:13 +0000

    add docstring of Jshell

--- src/driver/JShell.java
+++ src/driver/JShell.java
@@ -34,7 +34,16 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 import a2.*;
-
+/**
+ * Determines how similar each pair of users is based on their ratings. This
+ * similarity value is represented with with a float value between 0 and 1,
+ * where 1 is perfect/identical similarity. Stores these values in the
+ * userUserMatrix.
+ * 
+ * @param items1 is the column of the first user.
+ * @param items2 is the column of the second user.
+ * @return return a double represents the distance between users.
+ */
 public class JShell {
 
   // a boolean variable to control the status of JShell
