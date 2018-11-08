d21323f850a807a743383d8bae31596441546fef
xincheng <45149881+chengyu66@users.noreply.github.com>
2018-11-08 03:00:54 +0000

    Update format of Popd Class

--- src/a2/Popd.java
+++ src/a2/Popd.java
@@ -1,44 +1,46 @@
 package a2;
 
 
-public class Popd extends Command{
-	/**
-	 * This class gets the user to the most recent directory from 
-	 * the DirectoryStack. And changes the directory to that the top entry. 
-	 * 
-	 * @param fs which is a FileSystem
-	 * @return null
-	 */
-	public Popd(FileSystem fs) {
-		super(fs);
-	}
-	/**
-	 * This class gets the user to the most recent directory from 
-	 * the DirectoryStack. And changes the directory to that the top entry. 
-	 * 
-	 * @param fs which is a FileSystem
-	 * @return null
-	 */
-	public void run(String[] input) {
-		
-		// There is at least one Directory in the DirectoryStack
-		if (!(fs.getDirectoryStack().isEmpty())) {
-			// The directory on the top of the entry
-			Directory topDirectory = fs.getDirectoryStack().pop();
-			// change the directory to the top entry of DirectoryStack
-			fs.setCurrentDirectory(topDirectory);
-			
-		}
-		// No Directory in the stack
-		else {
-			// send a message to the user
-			System.out.println(EmptyDirectoryStackError());
-			
-		}
-	}
-	public String EmptyDirectoryStackError() {
-	    return "DirecotryStack is empty";
-	  }
+public class Popd extends Command {
+  /**
+   * This class gets the user to the most recent directory from the
+   * DirectoryStack. And changes the directory to that the top entry.
+   * 
+   * @param fs which is a FileSystem
+   * @return null
+   */
+  public Popd(FileSystem fs) {
+    super(fs);
+  }
+
+  /**
+   * This class gets the user to the most recent directory from the
+   * DirectoryStack. And changes the directory to that the top entry.
+   * 
+   * @param fs which is a FileSystem
+   * @return null
+   */
+  public void run(String[] input) {
+
+    // There is at least one Directory in the DirectoryStack
+    if (!(fs.getDirectoryStack().isEmpty())) {
+      // The directory on the top of the entry
+      Directory topDirectory = fs.getDirectoryStack().pop();
+      // change the directory to the top entry of DirectoryStack
+      fs.setCurrentDirectory(topDirectory);
+
+    }
+    // No Directory in the stack
+    else {
+      // send a message to the user
+      System.out.println(EmptyDirectoryStackError());
+
+    }
+  }
+
+  public String EmptyDirectoryStackError() {
+    return "DirecotryStack is empty";
+  }
 
 }
 
