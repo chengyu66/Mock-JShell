8ce146de39338315fcd73494a21ea65521677d48
mihao <mihao@UTORONTO.CA>
2018-11-08 02:56:10 +0000

    add the docstring

--- src/a2/File.java
+++ src/a2/File.java
@@ -1,31 +1,51 @@
 package a2;
 /**
- * Determines how similar each pair of users is based on their ratings. This
- * similarity value is represented with with a float value between 0 and 1,
- * where 1 is perfect/identical similarity. Stores these values in the
- * userUserMatrix.
- * 
- * @param items1 is the column of the first user.
- * @param items2 is the column of the second user.
- * @return return a double represents the distance between users.
+ * The class represents the file.
+ * It has the ability as what the file can do in the computer
+ * It can collect the content
  */
 public class File extends FileDirectory{
 	
 	private String content;
 	
+	/**
+	   * the constructor of file
+	   * initialize content
+	   * 
+	   * @param None
+	   * @return the String path of this FileDirectory.
+	   */
 	public File(String name, Directory parent){
 		super(name, parent);
 		this.content = "";
 	}
 
+	/**
+	   * output the content of the file
+	   * 
+	   * @param None
+	   * @return the String content
+	   */
 	public String getContent() {
 		return content;
 	}
-
+	
+	/**
+	   * rewrite the content of the file
+	   * 
+	   * @param the String content
+	   * @return None
+	   */
 	public void setContent(String content) {
 		this.content = content;
 	}
 	
+	/**
+	   * add the content.
+	   * 
+	   * @param the String content
+	   * @return None
+	   */
 	public void addContent(String content){
 		this.content += content;
 	}
