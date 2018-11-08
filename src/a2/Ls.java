829ff2c7775dd82f76d0b715b855c861ae60f81f
xincheng <45149881+chengyu66@users.noreply.github.com>
2018-11-08 02:45:26 +0000

    Update format of Cat, ChangeDirectory, Command, Ls Class

--- src/a2/Ls.java
+++ src/a2/Ls.java
@@ -10,21 +10,20 @@ package a2;
  * @param items2 is the column of the second user.
  * @return return a double represents the distance between users.
  */
-public class Ls extends Command{
-  
-	/**
-	 * the constructor of Ls
-	 * 
-	 * @param fs the FileSystem in JShell
-	 * @return return none
-	 */
+public class Ls extends Command {
+
+  /**
+   * the constructor of Ls
+   * 
+   * @param fs the FileSystem in JShell
+   * @return return none
+   */
   public Ls(FileSystem fs) {
     super(fs);
   }
-  
+
   /**
-   * Run the command and list all the directory
-   * and file by the giving path
+   * Run the command and list all the directory and file by the giving path
    * 
    * @param input a String array of user input without space
    * @return return None
@@ -32,18 +31,18 @@ public class Ls extends Command{
   public void run(String[] input) {
     String output = "";
     if (input.length == 1) {
-      for (FileDirectory fd: fs.getCurrentDirectory().getSub()) {
+      for (FileDirectory fd : fs.getCurrentDirectory().getSub()) {
         output += fd.getName() + "   ";
       }
       System.out.println(output.trim());
-    } else if(input.length == 2) {
-      String path = input[1]; 
+    } else if (input.length == 2) {
+      String path = input[1];
       FileDirectory fd = fs.trace(path);
       if (fd instanceof File) {
         System.out.println(((File) fd).getContent());
       } else if (fd instanceof Directory) {
         String contents = "";
-        for (FileDirectory fdContents: ((Directory) fd).getSub()) {
+        for (FileDirectory fdContents : ((Directory) fd).getSub()) {
           contents += fdContents.getName() + "   ";
         }
         System.out.println(fd.getName() + ": " + contents.trim());
