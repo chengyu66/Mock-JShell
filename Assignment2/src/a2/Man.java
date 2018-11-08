2a83378f72608600af690f42ea448566438bb0c0
xincheng@UTORONTO.CA <xincheng@UTORONTO.CA@a184c211-6917-47c9-80ea-094f3466f46b>
2018-11-08 03:11:20 +0000

    Update format of Man Class

--- Assignment2/src/a2/Man.java
+++ Assignment2/src/a2/Man.java
@@ -2,6 +2,7 @@ package a2;
 
 import java.util.Map;
 import a2.FileSystem;
+
 /**
  * Man class is a command to be construct
  * 
@@ -10,18 +11,20 @@ import a2.FileSystem;
  * @param fs is FileSystem to fit command format
  * @return return None
  */
-public class Man extends Command{
+public class Man extends Command {
   private Documentation doc;
   private Map<String, String> map;
+
   public Man(FileSystem fs) {
     super(fs);
     Documentation d = new Documentation();
     doc = d;
     map = doc.getMap();
   }
+
   /**
-   * run method return none but print the documentaiotn
-   * by the given command string
+   * run method return none but print the documentaiotn by the given command
+   * string
    * 
    * @param input is a string input list split by "/"
    * @return return None
@@ -30,25 +33,26 @@ public class Man extends Command{
     if (this.isValid(input)) {
       // tell if input command string not in the hashmap
       if (map.get(input[1]) == null) {
-      System.out.println(errorMessage());
-      }else {
-      // get its documentation
-      System.out.println(map.get(input[1]));
+        System.out.println(errorMessage());
+      } else {
+        // get its documentation
+        System.out.println(map.get(input[1]));
       }
     }
-    
+
   }
+
   public String errorMessage() {
     return "command not valid";
   }
-  
+
   public boolean isValid(String[] input) {
     boolean result = false;
     // tell if the length of the string input list split by "/"
     // is valid
-    if (input.length>=2) {
+    if (input.length >= 2) {
       result = true;
-    }else {
+    } else {
       result = false;
     }
     return result;
