f6f332b51e990274af476a788e28310cd8e9be59
xincheng@UTORONTO.CA <xincheng@UTORONTO.CA@a184c211-6917-47c9-80ea-094f3466f46b>
2018-11-08 03:10:35 +0000

    Update format of File Class

--- Assignment2/src/a2/File.java
+++ Assignment2/src/a2/File.java
@@ -1,53 +1,52 @@
 package a2;
-/**
- * The class represents the file.
- * It has the ability as what the file can do in the computer
- * It can collect the content
- */
-public class File extends FileDirectory{
-	
-	private String content;
-	
-	/**
-	   * the constructor of file
-	   * initialize content
-	   * 
-	   * @param name the name of FIle
-	   * @param parent the parent Directory of this file
-	   * @return the String path of this FileDirectory.
-	   */
-	public File(String name, Directory parent){
-		super(name, parent);
-		this.content = "";
-	}
 
-	/**
-	   * output the content of the file
-	   * 
-	   * @param None
-	   * @return the String content
-	   */
-	public String getContent() {
-		return content;
-	}
-	
-	/**
-	   * rewrite the content of the file
-	   * 
-	   * @param the String content
-	   * @return None
-	   */
-	public void setContent(String content) {
-		this.content = content;
-	}
-	
-	/**
-	   * add the content.
-	   * 
-	   * @param the String content
-	   * @return None
-	   */
-	public void addContent(String content){
-		this.content += content;
-	}
+/**
+ * The class represents the file. It has the ability as what the file can do in
+ * the computer It can collect the content
+ */
+public class File extends FileDirectory {
+
+  private String content;
+
+  /**
+   * the constructor of file initialize content
+   * 
+   * @param name the name of FIle
+   * @param parent the parent Directory of this file
+   * @return the String path of this FileDirectory.
+   */
+  public File(String name, Directory parent) {
+    super(name, parent);
+    this.content = "";
+  }
+
+  /**
+   * output the content of the file
+   * 
+   * @param None
+   * @return the String content
+   */
+  public String getContent() {
+    return content;
+  }
+
+  /**
+   * rewrite the content of the file
+   * 
+   * @param the String content
+   * @return None
+   */
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  /**
+   * add the content.
+   * 
+   * @param the String content
+   * @return None
+   */
+  public void addContent(String content) {
+    this.content += content;
+  }
 }
